d2b3e4d30ff503cafaeadc34799a4f67a2d7ee5a
JasonBMa <JasonBMa@users.noreply.github.com>
2024-03-05 18:20:46 -0800

    reorganized zombie animations, correctly implemtented animations, death animation ready to be used on zombies, sorry about messing up the spawn points and spawning

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -6,11 +6,12 @@ public class ZombieController : MonoBehaviour
 {
     public float speed = 3f;
     private Transform target;
-
+    private Animator animator;
     void Start()
     {
         // Find the player's transform using the tag "Player"
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
@@ -19,6 +20,7 @@ public class ZombieController : MonoBehaviour
             // Move the zombie towards the player
             Vector2 direction = (target.position - transform.position).normalized;
             transform.Translate(direction * speed * Time.deltaTime);
+            animator.SetFloat("Speed", speed);
         }
     }
 }
