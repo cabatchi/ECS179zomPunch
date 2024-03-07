26df3f106e954a96dd8f937b5cc8008450c0458d
ericbarron329 <ericbarron329@gmail.com>
2024-03-06 16:57:14 -0800

    Added Zombie Collisions

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -11,4 +11,16 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private float damage;
 
     float timeUntilMelee;
+
+    public void update() 
+    {
+private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Zombie") 
+        {
+            // other.GetComponent<Enemy>().TakeDamage(damage);
+            Debug.Log("Enemy hit");
+        }
+    }
+    }
 }
