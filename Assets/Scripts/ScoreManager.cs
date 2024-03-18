2f294ec248e8583cfafef2a65c1bf92cbb109638
keith-loww <keithlow1@hotmail.com>
2024-03-18 10:54:59 -0700

    Add score update functionality to ScoreManager

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private static int score;
+    public GameObject scoreText;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddScore(10);
+        }
+
+        scoreText.GetComponent<TMPro.TextMeshProUGUI>().text = "Score: " + score;
+    }
 
     public int GetScore()
     {
