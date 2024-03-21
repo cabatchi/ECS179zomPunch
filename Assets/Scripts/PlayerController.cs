53d3f9d6aa2757667432123a2f5fa4bec02efa8b
ericbarron329 <ericbarron329@gmail.com>
2024-03-21 13:54:27 -0700

    Bug fix with health

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,8 +30,6 @@ public class PlayerController : MonoBehaviour
     private Coroutine healingCoroutine;
     private bool canHeal = false;
     private float lastHitTime = 0f;
-    
-
     void Awake()
     {
         modifiedSpeed = baseSpeed;
@@ -41,12 +39,16 @@ public class PlayerController : MonoBehaviour
         damage = 1;
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         rb = GetComponent<Rigidbody2D>();
-        healingCoroutine = StartCoroutine(HealOverTime());
+        // healingCoroutine = StartCoroutine(HealOverTime());
         powerUpsController = FindObjectOfType<PlayerPowerUpsController>();
         if (powerUpsController != null)
         {
             // Start the healing coroutine if the power-ups controller is found
             healingCoroutine = StartCoroutine(HealOverTime());
+        }
+        else 
+        {
+            Debug.Log("Couldnt find power up controller...");
         }
     }
 
